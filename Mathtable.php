cb51bdeeb16df7f199cdc709f4ba96b03ac3005e
Agus Judistira <34447974+AgusJudistira@users.noreply.github.com>
2018-01-20 20:28:39 +0100

    Add files via upload

--- Mathtable.php
+++ Mathtable.php
@@ -4,10 +4,30 @@
     <meta charset="utf-8">
     <title>Math table</title>
     <style>
-        body, td, thead {
-            padding:12px;
+        th {
+            color: white;
+            background-color: blue;
+            padding: 12px;
+        }
+        
+        td, tr {
+            padding: 12px;
             font-family: monospace;
             font-size: 20px;
+        }
+        
+        td {
+            text-align: right;
+            padding-right: 24px;
+        }
+
+         
+        tr:nth-child(odd) {
+            background-color: aliceblue;
+        }
+
+        tr:nth-child(even) {
+            background-color: lightblue;
         }
         
     </style>
@@ -16,15 +36,16 @@
   <body>
       <table border="1">
         <?php
-            echo "<thead>";
+            echo "<thead><tr><th colspan='3'>";
             echo nl2br("PHP assignment by: Agus Judistira (Math table)\n");
-            echo "</thead>";
+            echo "</th></tr></thead>";
+          
             function createMathTable($howBig) {
 
               for($i = 1; $i <= $howBig; $i++) {
                   echo "<tr>";
                   for($j = 1; $j <= 3; $j++) {
-                    echo "<td >";
+                    echo "<td align='center'>";
                     echo pow($i, $j);
                     echo "</td>";                          
                   }
